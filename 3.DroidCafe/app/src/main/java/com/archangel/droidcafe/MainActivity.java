3ecd914df834d59ced8b427ca72a1ee1dffc0f33
Ahmed Ezzat <ahmed.ezzat176148@gmail.com>
2020-04-19 18:32:21 +0200

    Add menu items

--- 3.DroidCafe/app/src/main/java/com/archangel/droidcafe/MainActivity.java
+++ 3.DroidCafe/app/src/main/java/com/archangel/droidcafe/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -47,13 +48,31 @@ public class MainActivity extends AppCompatActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (item.getItemId()) {
+            case R.id.action_order:
+                displayToast(getString(R.string.action_order_message));
+                Intent orderIntent = new Intent(MainActivity.this, OrderActivity.class);
+                orderIntent.putExtra(FirstFragment.EXTRA_MESSAGE, FirstFragment.getOrderMessage());
+                startActivity(orderIntent);
+                return true;
+            case R.id.action_status:
+                displayToast(getString(R.string.action_status_message));
+                return true;
+            case R.id.action_favorites:
+                displayToast(getString(R.string.action_favorites_message));
+                return true;
+            case R.id.action_contact:
+                displayToast(getString(R.string.action_contact_message));
+                return true;
+            default:
+                // Do nothing
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void displayToast(String message) {
+        Toast.makeText(getApplicationContext(), message,
+                Toast.LENGTH_SHORT).show();
     }
 }
