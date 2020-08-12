8744e5df18fa908111b8461b9563655d578fd3cf
Ahmed Ezzat <ahmed.ezzat176148@gmail.com>
2020-08-12 10:27:05 +0200

    Initial Layout Setup

--- 7.1 SimpleAsyncTask/app/src/main/java/com/codelab/archangel/simpleasynctask/MainActivity.java
+++ 7.1 SimpleAsyncTask/app/src/main/java/com/codelab/archangel/simpleasynctask/MainActivity.java
@@ -3,6 +3,8 @@ package com.codelab.archangel.simpleasynctask;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button sleepButton = findViewById(R.id.sleep_button);
+        sleepButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
