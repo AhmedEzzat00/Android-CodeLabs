fbc6b27967d23411079366adfc5d7ef43cbb8ac7
Ahmed Ezzat <ahmed.ezzat176148@gmail.com>
2020-08-12 13:01:54 +0200

    Create an empty AsyncTask Class to handle the query

---
+++ 7.2 WhoWroteIt/app/src/main/java/com/codelab/archangel/whowroteit/FetchBook.java
@@ -0,0 +1,27 @@
+package com.codelab.archangel.whowroteit;
+
+import android.os.AsyncTask;
+import android.widget.TextView;
+
+import java.lang.ref.WeakReference;
+
+public class FetchBook extends AsyncTask<String, Void, String> {
+
+    private WeakReference<TextView> mTitleText;
+    private WeakReference<TextView> mAuthorText;
+
+    public FetchBook(WeakReference<TextView> mTitleText, WeakReference<TextView> mAuthorText) {
+        this.mTitleText = mTitleText;
+        this.mAuthorText = mAuthorText;
+    }
+
+    @Override
+    protected String doInBackground(String... strings) {
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(String s) {
+        super.onPostExecute(s);
+    }
+}
