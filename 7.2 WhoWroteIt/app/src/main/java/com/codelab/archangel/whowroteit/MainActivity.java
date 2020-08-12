4c4a75133f9b17a9da412815f69b79dd312e6235
Ahmed Ezzat <ahmed.ezzat176148@gmail.com>
2020-08-12 18:19:41 +0200

    Manage the network state and the empty search field case

--- 7.2 WhoWroteIt/app/src/main/java/com/codelab/archangel/whowroteit/MainActivity.java
+++ 7.2 WhoWroteIt/app/src/main/java/com/codelab/archangel/whowroteit/MainActivity.java
@@ -3,6 +3,8 @@ package com.codelab.archangel.whowroteit;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
@@ -28,17 +30,39 @@ public class MainActivity extends AppCompatActivity {
 
     public void searchBooks(View view) {
         String queryString = mBookInput.getText().toString();
-        new FetchBook(mTitleText, mAuthorText).execute(queryString);
-        // this will execute before the query even it's written after it because
-        // the query task run in another thread and take time to complete while the execution finished
-        mAuthorText.setText("");
-        mTitleText.setText(R.string.loading);
+
+        //Hide Keyboard
         InputMethodManager inputManager = (InputMethodManager)
                 getSystemService(Context.INPUT_METHOD_SERVICE);
 
         if (inputManager != null) {
             inputManager.hideSoftInputFromWindow(view.getWindowToken(),
                     InputMethodManager.HIDE_NOT_ALWAYS);
+        }
+
+        //Check Network connectivity
+        ConnectivityManager connectivityManager = (ConnectivityManager)
+                getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = null;
+        if (connectivityManager != null)
+            networkInfo = connectivityManager.getActiveNetworkInfo();
+
+        if (networkInfo != null && networkInfo.isConnected() && queryString.length() != 0) {
+            new FetchBook(mTitleText, mAuthorText).execute(queryString);
+            // this will execute before the query even it's written after it because
+            // the query task run in another thread and take time to complete while the execution finished
+            mAuthorText.setText("");
+            mTitleText.setText(R.string.loading);
+        } else {
+            //test for empty query
+            if (queryString.length() == 0) {
+                mAuthorText.setText("");
+                mTitleText.setText(R.string.no_search_term);
+                //connectivity issues
+            } else {
+                mAuthorText.setText("");
+                mTitleText.setText(R.string.no_network);
+            }
         }
     }
 }
