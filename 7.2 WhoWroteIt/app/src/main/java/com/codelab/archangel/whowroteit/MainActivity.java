528ecf93c2a9de65ae56c213c86554084778c9d5
Ahmed Ezzat <ahmed.ezzat176148@gmail.com>
2020-08-12 12:54:32 +0200

    Initial Commit for lesson 7.2 : Loader

---
+++ 7.2 WhoWroteIt/app/src/main/java/com/codelab/archangel/whowroteit/MainActivity.java
@@ -0,0 +1,18 @@
+package com.codelab.archangel.whowroteit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void searchBooks(View view) {
+    }
+}
