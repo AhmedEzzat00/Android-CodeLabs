182f3156634310f687ddb4dabc4e6934eca4deac
Ahmed Ezzat <ahmed.ezzat176148@gmail.com>
2020-08-12 11:16:55 +0200

    Save the text state when the ui changes

--- 7.1 SimpleAsyncTask/app/src/main/java/com/codelab/archangel/simpleasynctask/MainActivity.java
+++ 7.1 SimpleAsyncTask/app/src/main/java/com/codelab/archangel/simpleasynctask/MainActivity.java
@@ -1,5 +1,6 @@
 package com.codelab.archangel.simpleasynctask;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -10,12 +11,17 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     private TextView mTextView;
+    private static final String TEXT_STATE = "currentText";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mTextView = findViewById(R.id.text_id);
+        //Restore the previous states
+        if (savedInstanceState != null)
+            mTextView.setText(savedInstanceState.getString(TEXT_STATE));
+
         Button sleepButton = findViewById(R.id.sleep_button);
         sleepButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -27,5 +33,11 @@ public class MainActivity extends AppCompatActivity {
                 new SimpleAsyncTask(mTextView).execute();
             }
         });
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString(TEXT_STATE, mTextView.getText().toString());
     }
 }
