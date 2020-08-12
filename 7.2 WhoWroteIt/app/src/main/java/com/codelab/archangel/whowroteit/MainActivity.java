f9ba961c78fb91d12bccf44c201094bb86d3557f
Ahmed Ezzat <ahmed.ezzat176148@gmail.com>
2020-08-12 19:00:51 +0200

    Attach loader to activity and get activate it's functions

--- 7.2 WhoWroteIt/app/src/main/java/com/codelab/archangel/whowroteit/MainActivity.java
+++ 7.2 WhoWroteIt/app/src/main/java/com/codelab/archangel/whowroteit/MainActivity.java
@@ -1,6 +1,10 @@
 package com.codelab.archangel.whowroteit;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.loader.app.LoaderManager;
+import androidx.loader.content.Loader;
 
 import android.content.Context;
 import android.net.ConnectivityManager;
@@ -11,8 +15,13 @@ import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
+public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<String> {
+
+    private static final int LOADER_ID = 0;
     private EditText mBookInput;
     private TextView mTitleText;
     private TextView mAuthorText;
@@ -26,6 +35,10 @@ public class MainActivity extends AppCompatActivity {
         mTitleText = findViewById(R.id.titleText);
         mAuthorText = findViewById(R.id.authorText);
 
+        //Connect the activity to the loader
+        if (LoaderManager.getInstance(this).getLoader(LOADER_ID) != null) {
+            LoaderManager.getInstance(this).initLoader(LOADER_ID, null, this);
+        }
     }
 
     public void searchBooks(View view) {
@@ -48,9 +61,11 @@ public class MainActivity extends AppCompatActivity {
             networkInfo = connectivityManager.getActiveNetworkInfo();
 
         if (networkInfo != null && networkInfo.isConnected() && queryString.length() != 0) {
-            new FetchBook(mTitleText, mAuthorText).execute(queryString);
-            // this will execute before the query even it's written after it because
-            // the query task run in another thread and take time to complete while the execution finished
+
+            Bundle queryBundle = new Bundle();
+            queryBundle.putString("queryString", queryString);
+            LoaderManager.getInstance(this).restartLoader(LOADER_ID, queryBundle, this);
+
             mAuthorText.setText("");
             mTitleText.setText(R.string.loading);
         } else {
@@ -64,5 +79,65 @@ public class MainActivity extends AppCompatActivity {
                 mTitleText.setText(R.string.no_network);
             }
         }
+    }
+
+    @NonNull
+    @Override
+    public Loader<String> onCreateLoader(int id, @Nullable Bundle args) {
+        String queryString = "";
+        if (args != null) {
+            queryString = args.getString("queryString");
+        }
+        return new BookLoader(this, queryString);
+    }
+
+    @Override
+    public void onLoadFinished(@NonNull Loader<String> loader, String data) {
+        try {
+            JSONObject jsonObject = new JSONObject(data);
+            JSONArray itemsArray = jsonObject.getJSONArray("items");
+
+            int i = 0;
+            String title = null;
+            String authors = null;
+
+            while (i < itemsArray.length() && (authors == null && title == null)) {
+                // Get the current item information.
+                JSONObject book = itemsArray.getJSONObject(i);
+                JSONObject volumeInfo = book.getJSONObject("volumeInfo");
+
+                // Try to get the author and title from the current item,
+                // catch if either field is empty and move on.
+                try {
+                    title = volumeInfo.getString("title");
+                    authors = volumeInfo.getString("authors");
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+                // Move to the next item.
+                i++;
+            }
+            if (title != null && authors != null) {
+                mTitleText.setText(title);
+                mAuthorText.setText(authors);
+            } else {
+                mTitleText.setText(R.string.no_results);
+                mAuthorText.setText("");
+            }
+
+        } catch (JSONException e) {
+            // If onPostExecute does not receive a proper JSON string,
+            // update the UI to show failed results.
+            mTitleText.setText(R.string.no_results);
+            mAuthorText.setText("");
+            e.printStackTrace();
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onLoaderReset(@NonNull Loader<String> loader) {
+
     }
 }
