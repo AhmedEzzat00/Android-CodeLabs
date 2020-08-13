d3eee8f6d91bf6c86f82662fee725b12803ffeee
Ahmed Ezzat <ahmed.ezzat176148@gmail.com>
2020-08-13 16:29:25 +0200

    Setup Notification channel for later versions of Android (API 26)

--- 8.1 NotifyMe/app/src/main/java/com/codelab/archangel/notifyme/MainActivity.java
+++ 8.1 NotifyMe/app/src/main/java/com/codelab/archangel/notifyme/MainActivity.java
@@ -2,13 +2,49 @@ package com.codelab.archangel.notifyme;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button notifyButton;
+    private static final String PRIMARY_CHANNEL_ID = "primary_notification_channel";
+    private NotificationManager mNotifyManger;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        notifyButton = findViewById(R.id.notify_button);
+        notifyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sendNotification();
+            }
+        });
+    }
+
+    private void sendNotification() {
+
+    }
+
+    public void createNotificationChannel() {
+        mNotifyManger = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel notificationChannel = new NotificationChannel(
+                    PRIMARY_CHANNEL_ID,
+                    "Mascot Notification",
+                    NotificationManager.IMPORTANCE_HIGH);
+            notificationChannel.enableLights(true);
+            notificationChannel.setLightColor(Color.RED);
+            notificationChannel.enableVibration(true);
+            notificationChannel.setDescription("Notification from Mascot");
+            mNotifyManger.createNotificationChannel(notificationChannel);
+        }
     }
 }
