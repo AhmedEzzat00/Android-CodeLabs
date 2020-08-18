7542ba0e4cac0511a8ccde7093676ba024793b50
Ahmed Ezzat <ahmed.ezzat176148@gmail.com>
2020-08-18 22:46:28 +0200

    Schedule the work for Network Constrains

--- 8.3 NotificationScheduler/app/src/main/java/com/codelab/archangel/notificationscheduler/MainActivity.java
+++ 8.3 NotificationScheduler/app/src/main/java/com/codelab/archangel/notificationscheduler/MainActivity.java
@@ -3,12 +3,17 @@ package com.codelab.archangel.notificationscheduler;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.job.JobInfo;
+import android.app.job.JobScheduler;
+import android.content.ComponentName;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.RadioGroup;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private JobScheduler mScheduler;
+    private static final int JOB_ID = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,20 +23,34 @@ public class MainActivity extends AppCompatActivity {
     public void scheduleJob(View view) {
         RadioGroup networkOptions = findViewById(R.id.networkOptions);
         int selectedNetworkID = networkOptions.getCheckedRadioButtonId();
-        int selectedNetworkOption = JobInfo.NETWORK_TYPE_NONE;
+        int selectedNetworkType = JobInfo.NETWORK_TYPE_NONE;
         switch(selectedNetworkID){
             case R.id.noNetwork:
-                selectedNetworkOption = JobInfo.NETWORK_TYPE_NONE;
+                selectedNetworkType = JobInfo.NETWORK_TYPE_NONE;
                 break;
             case R.id.anyNetwork:
-                selectedNetworkOption = JobInfo.NETWORK_TYPE_ANY;
+                selectedNetworkType = JobInfo.NETWORK_TYPE_ANY;
                 break;
             case R.id.wifiNetwork:
-                selectedNetworkOption = JobInfo.NETWORK_TYPE_UNMETERED;
+                selectedNetworkType = JobInfo.NETWORK_TYPE_UNMETERED;
                 break;
         }
+        mScheduler= (JobScheduler) getSystemService(JOB_SCHEDULER_SERVICE);
+        ComponentName serviceName=new ComponentName(getPackageName(),NotificationJobService.class.getName());
+        JobInfo.Builder builder=new JobInfo.Builder(JOB_ID,serviceName);
+        builder.setRequiredNetworkType(selectedNetworkType);
+
+        JobInfo jobInfo=builder.build();
+        mScheduler.schedule(jobInfo);
+        Toast.makeText(this, "Job Scheduled, job will run when the constraints are met.",
+                Toast.LENGTH_SHORT).show();
     }
 
     public void cancelJobs(View view) {
+        if (mScheduler!=null){
+            mScheduler.cancelAll();
+            mScheduler=null;
+            Toast.makeText(this, "Jobs cancelled", Toast.LENGTH_SHORT).show();
+        }
     }
 }
