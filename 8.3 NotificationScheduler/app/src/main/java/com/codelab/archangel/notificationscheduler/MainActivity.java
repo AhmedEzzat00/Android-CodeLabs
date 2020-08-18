3c2892d4cf06da0cd56f6b42ce93efef098528ea
Ahmed Ezzat <ahmed.ezzat176148@gmail.com>
2020-08-18 19:52:33 +0200

    Get the selected network option

--- 8.3 NotificationScheduler/app/src/main/java/com/codelab/archangel/notificationscheduler/MainActivity.java
+++ 8.3 NotificationScheduler/app/src/main/java/com/codelab/archangel/notificationscheduler/MainActivity.java
@@ -2,7 +2,10 @@ package com.codelab.archangel.notificationscheduler;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.job.JobInfo;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioGroup;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void scheduleJob(View view) {
+        RadioGroup networkOptions = findViewById(R.id.networkOptions);
+        int selectedNetworkID = networkOptions.getCheckedRadioButtonId();
+        int selectedNetworkOption = JobInfo.NETWORK_TYPE_NONE;
+        switch(selectedNetworkID){
+            case R.id.noNetwork:
+                selectedNetworkOption = JobInfo.NETWORK_TYPE_NONE;
+                break;
+            case R.id.anyNetwork:
+                selectedNetworkOption = JobInfo.NETWORK_TYPE_ANY;
+                break;
+            case R.id.wifiNetwork:
+                selectedNetworkOption = JobInfo.NETWORK_TYPE_UNMETERED;
+                break;
+        }
+    }
+
+    public void cancelJobs(View view) {
     }
 }
