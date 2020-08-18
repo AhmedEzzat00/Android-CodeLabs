b129ee779641e33f0eaeca8ecaf8bb0d682157d5
Ahmed Ezzat <ahmed.ezzat176148@gmail.com>
2020-08-18 22:55:27 +0200

    Add Device Idle and Device Charging constraints

--- 8.3 NotificationScheduler/app/src/main/java/com/codelab/archangel/notificationscheduler/MainActivity.java
+++ 8.3 NotificationScheduler/app/src/main/java/com/codelab/archangel/notificationscheduler/MainActivity.java
@@ -8,16 +8,23 @@ import android.content.ComponentName;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.RadioGroup;
+import android.widget.Switch;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private JobScheduler mScheduler;
     private static final int JOB_ID = 0;
+
+    //Switches for setting idle and charging jon constrains
+    private Switch mDeviceIdleSwitch;
+    private Switch mDeviceChargingSwitch;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mDeviceIdleSwitch = findViewById(R.id.idleSwitch);
+        mDeviceChargingSwitch = findViewById(R.id.chargingSwitch);
     }
 
     public void scheduleJob(View view) {
@@ -38,12 +45,23 @@ public class MainActivity extends AppCompatActivity {
         mScheduler= (JobScheduler) getSystemService(JOB_SCHEDULER_SERVICE);
         ComponentName serviceName=new ComponentName(getPackageName(),NotificationJobService.class.getName());
         JobInfo.Builder builder=new JobInfo.Builder(JOB_ID,serviceName);
+
         builder.setRequiredNetworkType(selectedNetworkType);
 
-        JobInfo jobInfo=builder.build();
-        mScheduler.schedule(jobInfo);
-        Toast.makeText(this, "Job Scheduled, job will run when the constraints are met.",
-                Toast.LENGTH_SHORT).show();
+        builder.setRequiresDeviceIdle(mDeviceIdleSwitch.isChecked());
+        builder.setRequiresCharging(mDeviceChargingSwitch.isChecked());
+
+        //Check if there any constraints is set for the job
+        boolean constraintSet = (selectedNetworkType != JobInfo.NETWORK_TYPE_NONE)
+                || mDeviceChargingSwitch.isChecked() || mDeviceIdleSwitch.isChecked();
+        if (constraintSet){
+            JobInfo jobInfo=builder.build();
+            mScheduler.schedule(jobInfo);
+            Toast.makeText(this, "Job Scheduled, job will run when the constraints are met.",
+                    Toast.LENGTH_SHORT).show();
+        }else {
+            Toast.makeText(this, "Please set at least one constraint",Toast.LENGTH_SHORT).show();
+        }
     }
 
     public void cancelJobs(View view) {
