80c67245e5732ff58bfceaf09a6a18878c7cba27
Ahmed Ezzat <ahmed.ezzat176148@gmail.com>
2020-08-25 00:46:08 +0200

    Fix word Conflict (ensure Unique Keys for the words As it's PRIMARY_KEY)

--- 10. RoomWordsSample/app/src/main/java/com/codelab/archangel/roomwordssample/WordDAO.java
+++ 10. RoomWordsSample/app/src/main/java/com/codelab/archangel/roomwordssample/WordDAO.java
@@ -3,6 +3,7 @@ package com.codelab.archangel.roomwordssample;
 import androidx.lifecycle.LiveData;
 import androidx.room.Dao;
 import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
 import androidx.room.Query;
 
 import java.util.List;
@@ -10,7 +11,8 @@ import java.util.List;
 @Dao
 public interface WordDAO {
 
-    @Insert
+    //insure PRIMARY_KEY for the new Words
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
     void insert(Word word);
 
     @Query("DELETE FROM word_table")
