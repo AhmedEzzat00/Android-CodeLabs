ef6ad3133277767818fa075f98e24075366acd35
Ahmed Ezzat <ahmed.ezzat176148@gmail.com>
2020-08-24 23:31:29 +0200

    Create the ViewModel

---
+++ 10. RoomWordsSample/app/src/main/java/com/codelab/archangel/roomwordssample/WordViewModel.java
@@ -0,0 +1,38 @@
+package com.codelab.archangel.roomwordssample;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import java.util.List;
+
+/*
+    Warning:
+        Never pass context into ViewModel instances.
+        Do not store Activity, Fragment, or View instances or their Context in the ViewModel.
+
+        An Activity can be destroyed and created many times during the lifecycle of a ViewModel,
+        such as when the device is rotated.
+        If you store a reference to the Activity in the ViewModel,
+        you end up with references that point to the destroyed Activity
+ */
+public class WordViewModel extends AndroidViewModel {
+    private WordRepository mRepository;
+    private LiveData<List<Word>> mAllWords;
+
+    public WordViewModel(@NonNull Application application) {
+        super(application);
+        mRepository = new WordRepository(application);
+        mAllWords = mRepository.getAllWords();
+    }
+
+    LiveData<List<Word>> getAllWords() {
+        return mAllWords;
+    }
+
+    public void insert(Word word) {
+        mRepository.insert(word);
+    }
+}
